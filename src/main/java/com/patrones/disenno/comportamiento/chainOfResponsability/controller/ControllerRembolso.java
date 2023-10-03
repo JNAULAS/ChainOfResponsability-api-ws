367633cd92f3b5996ec8d596ea40e641f831c937
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-10-02 23:06:01 -0500

    Update datos vista

--- src/main/java/com/patrones/disenno/comportamiento/chainOfResponsability/controller/ControllerRembolso.java
+++ src/main/java/com/patrones/disenno/comportamiento/chainOfResponsability/controller/ControllerRembolso.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.patrones.disenno.comportamiento.chainOfResponsability.model.ManejadorDuenno;
+import com.patrones.disenno.comportamiento.chainOfResponsability.model.ManejadorGerente;
+import com.patrones.disenno.comportamiento.chainOfResponsability.model.ManejadorSupervisor;
 import com.patrones.disenno.comportamiento.chainOfResponsability.request.RequestChainOfResponsability;
 
 /**
@@ -34,6 +37,20 @@ public class ControllerRembolso {
 	// Accion para ejecutar solicitat rembolso
 	@PostMapping("/solicitarRembolso")
 	public String solicitarRembolso(@ModelAttribute RequestChainOfResponsability requestChainOfResponsability) {
+		RequestChainOfResponsability requestChainOfResponsabilitySet = new RequestChainOfResponsability();
+		requestChainOfResponsabilitySet.setMontoRembolso(requestChainOfResponsability.getMontoRembolso());
+		
+		ManejadorSupervisor manejadorSupervisor = new ManejadorSupervisor();
+		ManejadorGerente manejadorGerente = new ManejadorGerente();
+		ManejadorDuenno manejadorDuenno = new ManejadorDuenno();
+		
+		manejadorSupervisor.setSiguienteManejadorRembolso(manejadorGerente);
+		manejadorGerente.setSiguienteManejadorRembolso(manejadorDuenno);
+		
+		manejadorSupervisor.manjeadorSolicitudes(requestChainOfResponsabilitySet);
+		
+		requestChainOfResponsability.setResultadoRembolso(requestChainOfResponsabilitySet.getResultadoRembolso());
+		
 		return "redirect:/SolicitudRembolso";
 	}
 }
